5d08458f63d6564689cd16b7a155552437a6698c
Brian Farias <67401090+brianvfarias@users.noreply.github.com>
2023-03-08 22:36:06 -0300

    Atualizando resposta do teste 1 :sweat_smile:

---
+++ while.ts
@@ -0,0 +1,13 @@
+// Código adicionado após perceber que me enganei na resposta do primeiro teste!
+// Resposta que encontrei é 91
+
+const indice = 13;
+let soma = 0;
+let k = 0;
+
+while (k < indice) {
+  k = k + 1
+  soma = soma + k
+}
+
+console.log(soma)
